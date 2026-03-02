2add75544007e9d1963fcb634f44cd4168993ab4
spaul91 <sophie.paul3@nhs.net>
2026-03-02 11:42:27 +0000

    added aggregation tests

--- src/eggd_contaminant_detector/test/test_utils.py
+++ src/eggd_contaminant_detector/test/test_utils.py
@@ -1,6 +1,33 @@
-from string import ascii_lowercase
-from eggd_contaminant_detector.utils import get_file_id, shorten
+import pytest
+import pandas as pd
+from pathlib import Path
+from string import ascii_lowercase, ascii_uppercase
+from eggd_contaminant_detector.utils import get_file_id, read_csvs, shorten
 from eggd_contaminant_detector.types import DXFileID, DXProjectID, DXLink
+
+@pytest.fixture
+def csv_dfs() -> dict[str, pd.DataFrame]:
+    return {
+        "A": pd.DataFrame({"id": [0], "value": ["A"]}),
+        "B": pd.DataFrame({"id": [0], "value": ["B"]}),
+        "C": pd.DataFrame({"id": [0], "value": ["C"]}),
+    }
+
+@pytest.fixture()
+def flat_csv_dir(csv_dfs: list[pd.DataFrame], tmp_path: Path) -> Path:
+    d = tmp_path / "data"
+    d.mkdir(parents=True, exist_ok=True)
+    for key in csv_dfs:
+        csv_dfs[key].to_csv(d / f"{key}.csv", index=False)
+    return d
+
+@pytest.fixture()
+def nested_csv_dir(csv_dfs: list[pd.DataFrame], tmp_path: Path) -> Path:
+    d = tmp_path / "subdir" / "data"
+    d.mkdir(parents=True, exist_ok=True)
+    for key in csv_dfs:
+        csv_dfs[key].to_csv(d / f"{key}.csv", index=False)
+    return d
 
 def test_get_file_id() -> None:
     file_id: DXFileID = f"file-{ascii_lowercase}"
@@ -20,3 +47,21 @@ def test_shorten() -> None:
 
     assert shorten(epic_name) == epic_name_shortened
     assert shorten(non_epic_name) == non_epic_name
+
+def test_read_csvs_concatenates_correctly(flat_csv_dir: Path) -> None:
+    result = read_csvs(flat_csv_dir, "*.csv")
+    
+    assert len(result) == 3
+    assert set(result["value"]) == {"A", "B", "C"}
+    assert isinstance(result, pd.DataFrame)
+
+def test_read_csvs_concatenates_data_correctly(nested_csv_dir: Path) -> None:
+    result = read_csvs(nested_csv_dir, "**/*.csv")
+    
+    assert len(result) == 3
+    assert set(result["value"]) == {"A", "B", "C"}
+    assert isinstance(result, pd.DataFrame)
+
+def test_read_csvs_with_no_matches(flat_csv_dir: Path) -> None:
+    with pytest.raises(ValueError, match="No objects to concatenate"):
+        read_csvs(flat_csv_dir, "*.xlsx")
