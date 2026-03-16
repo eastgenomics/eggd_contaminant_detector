b73e157c1b53be1ea2de0e197239dda0e0c6a4a9
spaul91 <sophie.paul3@nhs.net>
2026-03-16 17:11:49 +0000

    abstracted out ref tar extraction

--- src/eggd_contaminant_detector/utils.py
+++ src/eggd_contaminant_detector/utils.py
@@ -13,22 +13,7 @@ def download_reference(reference: DXFileID, ref_index: Optional[DXFileID]=None, 
     ref_path: Path = destination / ref_name
     dxpy.download_dxfile(reference, filename=str(ref_path))
     if ref_path.name.endswith((".tar", ".tar.gz", ".tgz")):
-        with tarfile.open(ref_path) as tar:
-            members = tar.getmembers()
-            fasta_exts = (".fa", ".fasta", ".fna", ".fa.gz", ".fasta.gz")
-            index_exts = (".fai", ".gzi")
-            try:
-                fasta_m = next(m for m in members if m.name.lower().endswith(fasta_exts))
-                index_m = next(m for m in members if m.name.lower().endswith(index_exts))
-            except StopIteration:
-                raise FileNotFoundError(f"Tarball {ref_name} must contain both a FASTA and an index (.fai/.gzi)")
-            destination_root = destination.resolve()
-            for member in (fasta_m, index_m):
-                member_path = (destination_root / member.name).resolve()
-                if not member_path.is_relative_to(destination_root):
-                    raise FileNotFoundError(f"Unsafe path in tarball member: {member.name}")
-            tar.extractall(members=[fasta_m, index_m], path=destination)
-            ref_path = destination / fasta_m.name
+        ref_path = extract_ref_tar(ref_path, destination)
         if ref_index:
             print("WARNING: Tarball provided; ignoring the additional reference_index input.")
     else:
@@ -37,6 +22,28 @@ def download_reference(reference: DXFileID, ref_index: Optional[DXFileID]=None, 
         ref_index_name = dxpy.describe(ref_index)["name"]
         dxpy.download_dxfile(ref_index, str(destination / ref_index_name))
     return ref_path
+
+def extract_ref_tar(ref_path: Path, destination: Path) -> Path:
+    destination.mkdir(parents=True, exist_ok=True)
+    destination_root = destination.resolve()
+    with tarfile.open(ref_path) as tar:
+        members = tar.getmembers()
+        fasta_exts = (".fa", ".fasta", ".fna", ".fa.gz", ".fasta.gz")
+        index_exts = (".fai", ".gzi")
+        try:
+            fasta_m = next(m for m in members if m.name.lower().endswith(fasta_exts))
+            index_m = next(m for m in members if m.name.lower().endswith(index_exts))
+        except StopIteration:
+            raise FileNotFoundError(f"Tarball {ref_path.name} missing FASTA or index.")
+        for member in [fasta_m, index_m]:
+            # safety check (".."s in tar member names are unsafe)
+            original_path = (destination_root / member.name).resolve()
+            if not original_path.is_relative_to(destination_root):
+                raise PermissionError(f"Unsafe path detected: {member.name}")
+            # strip away the nesting from the Tarfile object names so we can extract them directly
+            member.name = Path(member.name).name
+        tar.extractall(members=[fasta_m, index_m], path=destination_root)
+        return destination_root / fasta_m.name
 
 def get_file_id(dx_link: DXLink) -> DXFileID:
     """Extracts the raw file ID string from a DNAnexus link object.
