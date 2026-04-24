82560c891c786d7556c7fa2adc537b977fcf0133
spaul91 <sophie.paul3@nhs.net>
2026-04-24 16:43:38 +0100

    fixed some minor issues

--- src/contaminant_detector/src/contaminant_detector/cli.py
+++ src/contaminant_detector/src/contaminant_detector/cli.py
@@ -4,29 +4,81 @@ from typing import Optional
 
 from . import run_contam_check, plot_recall
 
+class PathOrString(click.ParamType):
+    """This is a custom type that allows us to submit either a path or a string.
+    We want this because check supports submitting either a path to a docker image,
+    or the name of the docker image if you'd prefer to use a local/remote Docker registry"""
+    name = "path_or_string"
+
+    def convert(self, value, param, ctx):
+        if value is None:
+            return None
+        
+        p = Path(value)
+        if p.exists():
+            return p.resolve()
+        
+        return value
+
+
 @click.group()
 def main():
-    """Runs contamination detection by comparing recall values across groups of contaminated samples and potential candidates.
-    """
+    """Runs contamination detection by comparing recall values across groups of contaminated samples and potential candidates."""
     pass
 
+
 @main.command()
-@click.option('--truth', '-t', multiple=True, type=click.Path(exists=True, path_type=Path, resolve_path=True))
-@click.option('--query', '-q', multiple=True, type=click.Path(exists=True, path_type=Path, resolve_path=True))
-@click.option('--reference', '-r', type=click.Path(exists=True, path_type=Path, resolve_path=True))
-@click.option('--ref-index', '-i', type=click.Path(exists=False, path_type=Path, resolve_path=True))
-@click.option('--out-dir', '-o', default=Path("."), type=click.Path(exists=False, path_type=Path, resolve_path=True))
-@click.option('--sompy-image', '-s', type=(click.Path(exists=True, path_type=Path, resolve_path=True), str))
-@click.option('--bcftools-image', '-b', type=(click.Path(exists=True, path_type=Path, resolve_path=True), str))
-@click.option('--preprocess', '-p', is_flag=True, default=True)
+@click.option(
+    "--truth",
+    "-t",
+    multiple=True,
+    type=click.Path(exists=True, path_type=Path, resolve_path=True),
+)
+@click.option(
+    "--query",
+    "-q",
+    multiple=True,
+    type=click.Path(exists=True, path_type=Path, resolve_path=True),
+)
+@click.option(
+    "--reference", "-r", type=click.Path(exists=True, path_type=Path, resolve_path=True)
+)
+@click.option(
+    "--ref-index",
+    "-i",
+    type=click.Path(exists=False, path_type=Path, resolve_path=True),
+)
+@click.option(
+    "--panel",
+    "-p",
+    type=click.Path(exists=False, path_type=Path, resolve_path=True)
+)
+@click.option(
+    "--out-dir",
+    "-o",
+    default=Path("."),
+    type=click.Path(exists=False, path_type=Path, resolve_path=True),
+)
+@click.option(
+    "--sompy-image",
+    "-s",
+    type=PathOrString(),
+)
+@click.option(
+    "--bcftools-image",
+    "-b",
+    type=PathOrString(),
+)
+@click.option("--normalise", "-n", is_flag=True, default=True)
 def check(
     truth: tuple[Path],
     query: tuple[Path],
     reference: Path,
+    panel: Path,
     out_dir: Path,
     sompy_image: str | Path,
     bcftools_image: str | Path,
-    preprocess: bool,
+    normalise: bool,
     ref_index: Optional[Path],
 ):
     """Runs checks"""
@@ -34,18 +86,27 @@ def check(
         truths=[*truth],
         querys=[*query],
         reference=reference,
+        panel_regions=panel,
         out_dir=out_dir,
         sompy_image=sompy_image,
         bcftools_image=bcftools_image,
         ref_index=ref_index,
-        preprocess=preprocess
+        preprocess=normalise,
     )
     click.echo(f"Results written to {out_dir}")
 
+
 @main.command()
-@click.option('--in', '-i', "input", type=click.Path(exists=True, path_type=Path, resolve_path=True))
-@click.option('--out', '-o', type=click.Path(exists=True, path_type=Path, resolve_path=True))
-@click.option('--baseline', '-b', type=float)
+@click.option(
+    "--in",
+    "-i",
+    "input",
+    type=click.Path(exists=True, path_type=Path, resolve_path=True),
+)
+@click.option(
+    "--out", "-o", type=click.Path(exists=True, path_type=Path, resolve_path=True)
+)
+@click.option("--baseline", "-b", type=float)
 def plot(input: Path, out: Path, baseline: float):
     """runs plots"""
     plot_recall(in_dir=input, out_dir=out, baseline=baseline)
